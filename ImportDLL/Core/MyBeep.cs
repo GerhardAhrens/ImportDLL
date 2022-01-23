addb1bd59fd3dc0fd3cfbdf704f67cedd3183153
GerhardAhrens <gerhard.ahrens@lifeprojects.de>
2022-01-23 17:54:21 +0100

    Projektdateien hinzufügen.

---
+++ ImportDLL/Core/MyBeep.cs
@@ -0,0 +1,46 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="MyBeep.cs" company="Lifeprojects.de">
+//     Class: MyBeep
+//     Copyright © Lifeprojects.de 2022
+// </copyright>
+//
+// <author>Gerhard Ahrens - Lifeprojects.de</author>
+// <email>gerhard.ahrens@lifeprojects.de</email>
+// <date>22.02.2022</date>
+//
+// <summary>
+// Klasse für 
+// </summary>
+//-----------------------------------------------------------------------
+
+
+namespace ImportDLL.Core
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    public class MyBeep
+    {
+        private delegate int BeepDelegate(int frequenz, int dauer);
+
+        private static BeepDelegate internalBeep;
+
+        static MyBeep()
+        {
+            try
+            {
+                internalBeep = DLLFunctionLoader.LoadFunction<BeepDelegate>("kernel32", "Beep");
+            }
+            catch (Exception ex)
+            {
+                string errText = ex.Message;
+                throw ex;
+            }
+        }
+
+        public void Play(int frequenz, int dauer = 400)
+        {
+            internalBeep(frequenz, dauer);
+        }
+    }
+}
---
+++ ImportDLL/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using ImportDLL.Core;
+
+namespace DelegateImportDLL
+{
+    public class Program
+    {
+
+        private static void Main(string[] args)
+        {
+
+            uint pMsgTyp = (uint)MsgOptions.MB_OK | (uint)MsgOptions.MB_ICONASTERISK;
+            int result = SystemMessageBox.Show("MsgBox Text\nÜber ein Delegate aufgerufenen Win32 API Funktion", "Titel", pMsgTyp);
+
+            MyBeep beep = new MyBeep();
+            for (int i = 0; i < 3; i++)
+            {
+                beep.Play(500);
+                beep.Play(1000);
+            }
+
+        }
+    }
+}
